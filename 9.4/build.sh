078a29299899ecbc45b6b7d5b55a35dcc15726d0
zhouyq <zhouyq@goodrain.com>
2017-06-04 22:32:22 +0800

    调整makefile

--- 9.4/build.sh
+++ 9.4/build.sh
@@ -9,4 +9,7 @@ cd /tmp/postgresql-* && \
     ./configure && \
     make && \
     make install  && \
+    cd /usr/local/pgsql/ && \
+    tar czvf $PG_VER.tar.gz * && \
+    mv $PG_VER.tar.gz ../ && \
     echo "PostgreSQL build completed!"
